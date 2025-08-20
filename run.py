b16aa36f5f0cb1111565c768b69efed42e641350
Vincent Li <vincentli0921@gmail.com>
2025-08-19 18:35:15 -0700

    changed the llm models

--- run.py
+++ run.py
@@ -85,13 +85,13 @@ def parse_args():
     parser.add_argument(
         "--opt_model_name",
         type=str,
-        default="claude-3-5-sonnet-20241022",
+        default="gpt-4o",
         help="Specifies the name of the model used for optimization tasks.",
     )
     parser.add_argument(
         "--exec_model_name",
         type=str,
-        default="gpt-4o-mini",
+        default="gpt-4o",
         help="Specifies the name of the model used for execution tasks.",
     )
     return parser.parse_args()
